af9a5cba76c127e826c8156cb69bbfa87f5c665a
Milan <kokinamilanko@gmail.com>
2026-03-23 18:47:50 +0100

    pridanie flask-cors

--- backend.py
+++ backend.py
@@ -1,49 +1,46 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+from flask_cors import CORS  # Umožňuje prepojenie s tvojím JavaScriptom
 
 app = Flask(__name__)
+CORS(app)  # Povolíme prístup z iných adries (kvôli bonusu)
 
-# Tvoja databáza
-databaza = [
-    {"id": 1, "name": "Adrian", "surname": "Cervenka", "nickname": "chilli pepper", "image": "https://picsum.photos/id/10/200"},
-    {"id": 2, "name": "Milan", "surname": "Kokina", "nickname": "tanecník", "image": "https://picsum.photos/id/20/200"},
-    {"id": 3, "name": "Martin", "surname": "Jelínek", "nickname": "král jelimán", "image": "https://picsum.photos/id/30/200"},
-    {"id": 4, "name": "Daniel", "surname": "Barta", "nickname": "skeleton", "image": "https://picsum.photos/id/40/200"},
-    {"id": 5, "name": "Jana", "surname": "Veselá", "nickname": "slniečko", "image": "https://picsum.photos/id/50/200"},
-    {"id": 6, "name": "Peter", "surname": "Horský", "nickname": "vlk", "image": "https://picsum.photos/id/60/200"},
-    {"id": 7, "name": "Lucia", "surname": "Biela", "nickname": "snežienka", "image": "https://picsum.photos/id/70/200"},
-    {"id": 8, "name": "Marek", "surname": "Tichý", "nickname": "duch", "image": "https://picsum.photos/id/80/200"},
-    {"id": 9, "name": "Simona", "surname": "Rýchla", "nickname": "raketa", "image": "https://picsum.photos/id/90/200"},
-    {"id": 10, "name": "Jakub", "surname": "Kováč", "nickname": "kladivo", "image": "https://picsum.photos/id/100/200"}
-]
+# 1. Databáza študentov (10 záznamov s obrázkami z internetu)
+databaza = {
+    "students": [
+        {"id": 1, "name": "Adrian", "surname": "Cervenka", "nickname": "chilli pepper", "image": "https://picsum.photos/id/1011/300/200"},
+        {"id": 2, "name": "Milan", "surname": "Kokina", "nickname": "tanecník", "image": "https://picsum.photos/id/1012/300/200"},
+        {"id": 3, "name": "Martin", "surname": "Jelínek", "nickname": "král jelimán", "image": "https://picsum.photos/id/1013/300/200"},
+        {"id": 4, "name": "Daniel", "surname": "Barta", "nickname": "skeleton", "image": "https://picsum.photos/id/1014/300/200"},
+        {"id": 5, "name": "Elena", "surname": "Kováčová", "nickname": "perla", "image": "https://picsum.photos/id/1015/300/200"},
+        {"id": 6, "name": "Peter", "surname": "Sekerka", "nickname": "drevorubač", "image": "https://picsum.photos/id/1016/300/200"},
+        {"id": 7, "name": "Jana", "surname": "Malá", "nickname": "včielka", "image": "https://picsum.photos/id/1018/300/200"},
+        {"id": 8, "name": "Marek", "surname": "Sloboda", "nickname": "vták", "image": "https://picsum.photos/id/1019/300/200"},
+        {"id": 9, "name": "Simona", "surname": "Vysoká", "nickname": "žirafa", "image": "https://picsum.photos/id/1020/300/200"},
+        {"id": 10, "name": "Lukáš", "surname": "Novák", "nickname": "blesk", "image": "https://picsum.photos/id/1021/300/200"}
+    ]
+}
 
-
-
-# Hlavná stránka - názov funkcie: home
+# 2. Route pre domovskú stránku (/)
 @app.route('/')
 def home():
-    return jsonify({"message": "Vitaj v mojom Flask backende!"})
+    return jsonify({"message": "Vitaj v mojom prvom Flask backende!"})
 
-# Zobrazenie všetkých - názov funkcie: get_api
+# 3. Route pre všetkých študentov (/api)
 @app.route('/api')
-def get_api():
-    return jsonify(databaza)
+def get_all_students():
+    # Vracia celé pole študentov
+    return jsonify(databaza["students"])
 
-# Vyhľadávanie podľa ID - názov funkcie: get_student_by_id
+# 4. Route pre jedného študenta podľa ID (/api/student/X)
 @app.route('/api/student/<int:student_id>')
 def get_student_by_id(student_id):
+    # Prechádzame zoznam a hľadáme zhodné ID
     for student in databaza["students"]:
         if student["id"] == student_id:
             return jsonify(student)
-    return jsonify({"error": "Študent sa nenašiel"}), 404
-
-# POST endpoint na dáta - názov funkcie: post_data
-@app.route('/data', methods=['POST'])
-def post_data():
-    vstup = request.get_json()
-    if not vstup:
-        return jsonify({"error": "Žiadne dáta"}), 400
-    return jsonify({"status": "prijaté", "data": vstup}), 201
+    
+    # Ak sa ID nenájde, vráti chybu a kód 404
+    return jsonify({"error": "Student not found"}), 404
 
 if __name__ == '__main__':
-    # Spustenie servera
     app.run(debug=True)
