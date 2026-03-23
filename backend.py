343fde7904f9450d7a4aba206bee8530cab15590
Milan <kokinamilanko@gmail.com>
2026-03-23 09:29:37 +0100

    23.3

---
+++ backend.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Vitaj v mojom Flask backende!"})
+
+
+databaza = {
+    "students": [
+        {
+            "id": 1,
+            "name": "Adrian",
+            "surname": "Cervenka",
+            "nickname": "chilli pepper"
+        },{
+             "id": 2,
+            "name": "Milan",
+            "surname": "Kokina",
+            "nickname": "tanecník"
+        },{
+             "id": 3,
+            "name": "Martin",
+            "surname": "Jelínek",
+            "nickname": "král jelimán"
+        },{
+             "id": 4,
+            "name": "Daniel",
+            "surname": "Barta",
+            "nickname": "skeleton"
+        }
+    ]
+}
+
+@app.route('/api')
+def api():
+    return jsonify(databaza)
+
+@app.route('/api/student/<int:student_id>')
+def find_student(student_id):
+    student = databaza["students"][student_id - 1]
+    return jsonify(student)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. POST požiadavka (odosielanie dát na server)
+@app.route('/data', methods=['POST'])
+def spracuj_data():
+    vstup = request.get_json() # Získame JSON dáta od klienta
+    
+    if not vstup:
+        return jsonify({"error": "Neposlal si žiadne dáta"}), 400
+    
+    return jsonify({
+        "status": "úspech",
+        "prijate_data": vstup
+    }), 201
+
+if __name__ == '__main__':
+    app.run(debug=True) 
