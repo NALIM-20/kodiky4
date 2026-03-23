c007b2fe4bad95171d14674562188846f2a175f3
Milan <kokinamilanko@gmail.com>
2026-03-23 18:22:58 +0100

    skuska5

--- backend.py
+++ backend.py
@@ -1,71 +1,44 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "Vitaj v mojom Flask backende!"})
-
-
+# Tvoja databáza
 databaza = {
     "students": [
-        {
-            "id": 1,
-            "name": "Adrian",
-            "surname": "Cervenka",
-            "nickname": "chilli pepper"
-        },{
-             "id": 2,
-            "name": "Milan",
-            "surname": "Kokina",
-            "nickname": "tanecník"
-        },{
-             "id": 3,
-            "name": "Martin",
-            "surname": "Jelínek",
-            "nickname": "král jelimán"
-        },{
-             "id": 4,
-            "name": "Daniel",
-            "surname": "Barta",
-            "nickname": "skeleton"
-        }
+        {"id": 1, "name": "Adrian", "surname": "Cervenka", "nickname": "chilli pepper"},
+        {"id": 2, "name": "Milan", "surname": "Kokina", "nickname": "tanecník"},
+        {"id": 3, "name": "Martin", "surname": "Jelínek", "nickname": "král jelimán"},
+        {"id": 4, "name": "Daniel", "surname": "Barta", "nickname": "skeleton"}
     ]
 }
 
-# 1. Hlavná stránka
+# Hlavná stránka - názov funkcie: home
 @app.route('/')
 def home():
     return jsonify({"message": "Vitaj v mojom Flask backende!"})
 
-# 2. Zobrazenie všetkých študentov (/api)
+# Zobrazenie všetkých - názov funkcie: get_api
 @app.route('/api')
-def get_all_students():
+def get_api():
     return jsonify(databaza)
 
-# 3. Vyhľadávanie podľa ID (/api/student/1)
+# Vyhľadávanie podľa ID - názov funkcie: get_student_by_id
 @app.route('/api/student/<int:student_id>')
-def find_student(student_id):
+def get_student_by_id(student_id):
     for student in databaza["students"]:
         if student["id"] == student_id:
             return jsonify(student)
-    
-    # Ak sa nenájde, vráti chybu
-    return jsonify({"error": "Student not found"}), 404
+    return jsonify({"error": "Študent sa nenašiel"}), 404
 
-# 4. Pridávanie nových dát (POST)
+# POST endpoint na dáta - názov funkcie: post_data
 @app.route('/data', methods=['POST'])
-def spracuj_data():
+def post_data():
     vstup = request.get_json()
     if not vstup:
-        return jsonify({"error": "Neposlal si žiadne dáta"}), 400
-    
-    return jsonify({
-        "status": "úspech",
-        "prijate_data": vstup
-    }), 201
+        return jsonify({"error": "Žiadne dáta"}), 400
+    return jsonify({"status": "prijaté", "data": vstup}), 201
 
 if __name__ == '__main__':
+    # Spustenie servera
     app.run(debug=True)
-
-
-
+    
