af5806dab8de285ecec36a41ce570e1e56559a6b
NALIM-20 <kokinamilanko@gmail.com>
2026-03-26 17:10:37 +0100

    Update student images with new URLs

--- backend.py
+++ backend.py
@@ -8,8 +8,8 @@ CORS(app)  # Povolíme prístup z iných adries (kvôli bonusu)
 databaza = {
     "students": [
     {"id": 1, "name": "Adrian", "surname": "Červenka", "nickname": "chilli pepper", "image": "https://picsum.photos/id/1011/300/200"},
-    {"id": 2, "name": "Milan", "surname": "Kokina", "nickname": "tanečník", "image": "https://sportnet.sme.sk/futbalnet/clen/5efee63f1b04f230d150c5ce/milan-kokina/"},
-    {"id": 3, "name": "Martin", "surname": "Jelínek", "nickname": "král jelimán", "image": "https://picsum.photos/id/1013/300/200"},
+    {"id": 2, "name": "Milan", "surname": "Kokina", "nickname": "tanečník", "image": "https://api.sportnet.online/v1/ppo/futbalsfz.sk/users/5efee63f1b04f230d150c5ce/formal-photo/e18f5e4d-9a8d-4196-9e18-30ebf1b60dc4"},
+    {"id": 3, "name": "Martin", "surname": "Jelínek", "nickname": "král jelimán", "image": "https://api.sportnet.online/v1/ppo/futbalsfz.sk/users/68c9112594d10f7e9dd591c4/formal-photo/94387b0f-c431-49e2-b562-6a357f415c2d"},
     {"id": 4, "name": "Daniel", "surname": "Barta", "nickname": "skeleton", "image": "https://picsum.photos/id/1014/300/200"},
     {"id": 5, "name": "Matej", "surname": "Randziak", "nickname": "tankista", "image": "https://picsum.photos/id/1015/300/200"},
     {"id": 6, "name": "Matúš", "surname": "Bucko", "nickname": "xxxxxxxxxx", "image": "https://picsum.photos/id/1016/300/200"},
